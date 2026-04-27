fc18a39e6812b672899f5efbd5a3364f33f305b5
Novatravel <joseperez120698@gmail.com>
2026-04-27 10:21:44 -0400

    Remove comment regarding repository name adjustment
    Removed comment about adjusting repository name.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  // Ajusta 'novatravel-cuba' si el nombre de tu repositorio es diferente
   base: '/novatravel-cuba/', 
   plugins: [
     react(),
